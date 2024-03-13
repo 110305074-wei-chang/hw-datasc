4f849f5fc59abfb1c0730f5ee8833a969462f18c
110305074-wei-chang <163027804+110305074-wei-chang@users.noreply.github.com>
2024-03-13 22:23:22 +0800

    Update hw1.R

--- hw1.R
+++ hw1.R
@@ -4,7 +4,7 @@ summary <- function(input, output) {
   max_體重 <- max(data$weight)
   max <- data[which(data$height == max_身高 & data$weight == max_體重), ]
   
-  max_summary <- data.frame(set = basename(input), weight = round(max_體重, 2), height = round(max身高, 2))
+  max_summary <- data.frame(set = basename(input), weight = round(max_體重, 2), height = round(max_身高, 2))
   write.csv(max_summary, file = output, row.names = FALSE)
   
   return(max_summary)
