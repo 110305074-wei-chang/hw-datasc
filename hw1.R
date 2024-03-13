65db0342e9601b5ecce692990165fa65e1b37a70
110305074-wei-chang <163027804+110305074-wei-chang@users.noreply.github.com>
2024-03-13 22:32:28 +0800

    Update hw1.R

--- hw1.R
+++ hw1.R
@@ -2,14 +2,11 @@ summary <- function(input, output) {
   data <- read.csv(input)
   max_身高 <- max(data$height)
   max_體重 <- max(data$weight)
-  max <- data[which(data$height == max_身高 & data$weight == max_體重), ]
   
   max_summary <- data.frame(set = basename(input), weight = round(max_體重, 2), height = round(max_身高, 2))
   write.csv(max_summary, file = output, row.names = FALSE)
   
   return(max_summary)
-  
-
 }
 
 # You could unmark the following cmd for local testing. Please mark it as a comment when uploading to GradeScope.
